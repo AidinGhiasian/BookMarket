ff937873bcec2e98526811a34a1057613f8f74ea
mobocom <mobocom@DESKTOP-GSJU6LT>
2025-12-04 19:12:02 +0330

    finish comment configuration and start comment application

---
+++ CommentM.Infrastructure.Configuration/CommentMInfrastructureConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommentM.Domain.Comment.AD;
+using CommentM.Infrastructure.EFCore;
+using CommentM.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommentM.Infrastructure.Configuration
+{
+    public class CommentMInfrastructureConfiguration
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<ICommentRepository,CommentRepository>();
+
+
+            services.AddDbContext<CommentDbContext>(options=>options.UseSqlServer(connectionString));
+        }
+          
+    }
+}
