bf211436373fb992e7e79aed3177d15a3b0c0f0f
mobocom <mobocom@DESKTOP-GSJU6LT>
2025-12-06 17:06:52 +0330

    finish Comment Application Contacts and start Account Aplication

---
+++ CommentM.Application.Contacts/EditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentM.Application.Contacts
+{
+    public class EditViewModel
+    {
+        public string FullName { get; set; }
+        public string Message { get; set; }
+        public DateTime CommentDateTime { get; set; }= DateTime.Now;
+    }
+}
---
+++ CommentM.Application.Contacts/ICommentApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentM.Application.Contacts
+{
+    public interface ICommentApplication
+    {
+        public void create(CreateViewModel create);
+        public void Edit(EditViewModel edit);
+        public CommentViewModel GetBy(long id);
+        public List<CommentViewModel> GetAll();
+        public void Delete(long id);
+    }
+}
