7a8a94de103da07f63e2c9f940ee3340e334b1d4
mobocom <mobocom@DESKTOP-GSJU6LT>
2025-12-13 15:20:06 +0330

    impeliment and finish ieventrepository and start impeliment eventrepository.....

---
+++ Blog.Domain/BlogAD/IEventRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services;
+
+namespace Blog.Domain.BlogAD
+{
+    public  interface IEventRepository:IRepositoryBase<Events>
+    {
+        public void Create(Events events);
+        public Events? GetById(long id);
+        public void  Update(Events events);
+        public void Delete(long Id);
+        public Events GetBy(string eventtitle);
+        public Events GetAll();
+    }
+}
