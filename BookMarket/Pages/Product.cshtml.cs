d97fae91184e1e74c25901c778cb4ce276124259
mobocom <mobocom@DESKTOP-GSJU6LT>
2026-01-21 18:41:30 +0330

    create some razor page....

---
+++ BookMarket/Pages/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookMarket.Pages
+{
+    public class PruductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BookMarket/Pages/WishList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookMarket.Pages
+{
+    public class WishListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
