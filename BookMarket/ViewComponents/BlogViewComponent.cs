766b3ac4068997467a7e777ad027861342e05047
mobocom <mobocom@DESKTOP-GSJU6LT>
2026-02-22 20:56:43 +0330

    Impelement View Component

---
+++ BookMarket/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BookM.ClientQueries.Model.Blog.Post;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMarket.ViewComponents
+{
+    public class BlogViewComponent:ViewComponent
+    {
+        private readonly IPostQueries _postQueries;
+        public BlogViewComponent( IPostQueries postQueries)
+        {
+            _postQueries = postQueries;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var post = _postQueries.GetAll();
+            return View(post);
+        }
+    }
+}
---
+++ BookMarket/ViewComponents/BookViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using BookM.ClientQueries.Model.Book.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMarket.ViewComponents
+{
+    
+    public class BookViewComponent:ViewComponent
+    {
+
+        private readonly IBookQueries _bookQueries;
+        public BookViewComponent(IBookQueries bookQueries)
+        {
+            _bookQueries = bookQueries;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var book = _bookQueries.GetAll();
+
+            return View(book);
+        }
+
+    }
+}
