cee96bfd1883446d4481734bba6523b06c224b1a
AviniIvar <AviniIvar@DESKTOP-Q8PIJ1C>
2026-02-27 23:54:29 +0330

    impelement BlogCategory View Component

---
+++ BookMarket/ViewComponents/BlogCategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BookM.ClientQueries.Model.Blog.Categores;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace BookMarket.ViewComponents
+{
+    public class BlogCategoryViewComponent:ViewComponent
+    {
+        private readonly IBlogCategoryQueries _blogCategoryQueries;
+        public BlogCategoryViewComponent(IBlogCategoryQueries blogCategoryQueries)
+        {
+            _blogCategoryQueries=blogCategoryQueries;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var category=_blogCategoryQueries.GetAll().Take(10).ToList();
+            return View(category);
+        }
+    }
+}
