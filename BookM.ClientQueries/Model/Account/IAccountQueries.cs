64354b30b4b429a4179c42b2b4459de83e66d9b1
AviniIvar <AviniIvar@DESKTOP-Q8PIJ1C>
2026-07-21 00:33:41 +0330

    change password refactor

--- BookM.ClientQueries/Model/Account/IAccountQueries.cs
+++ BookM.ClientQueries/Model/Account/IAccountQueries.cs
@@ -15,6 +15,5 @@ namespace BookM.ClientQueries.Model.Account
         public EditViewModel GetDetail(string phone);
         public OperationResult Login(string? phone, string? password);
         OperationResult Register(AccountM.Application.Contracts.AccountApplication.CreateViewModel model);
-        public OperationResult ChanagePassword()
     }
 }
