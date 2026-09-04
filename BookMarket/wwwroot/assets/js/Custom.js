7f29127aed86ffba0df83bebe3e3220287293543
Aidin Ghiasian <www.ghiasian.aidin@gmail.com>
2026-09-04 18:22:01 +0330

    get cookie in to cart items and we can remove thats finished cookie and now we have to go to create continue shpping...

--- BookMarket/wwwroot/assets/js/Custom.js
+++ BookMarket/wwwroot/assets/js/Custom.js
@@ -1,23 +1,25 @@
 ﻿const cookieName = "cart-items";
-function addToCart(id, bookTitle, price, pictureFile)
-{
+
+function addToCart(id, bookTitle, price, pictureFile) {
+
     let products = $.cookie(cookieName);
-    debugger;
-    if (products === undefined)
-    {
+
+    if (products === undefined) {
         products = [];
-    } else
-    {
+    }
+    else {
         products = JSON.parse(products);
     }
 
-    const count = $("#prudoctCount").val();
+    const count = $("#productCount").val();
 
     const currentProduct = products.find(x => x.id == id);
 
     if (currentProduct !== undefined) {
-        products.find(x => x.id === id).count = parseInt(currentProduct.count)+ parseInt(count);
-    } else {
+        products.find(x => x.id == id).count =
+            parseInt(currentProduct.count) + parseInt(count);
+    }
+    else {
         const product = {
             id,
             bookTitle,
@@ -33,5 +35,71 @@ function addToCart(id, bookTitle, price, pictureFile)
         expires: 2,
         path: "/"
     });
+
     console.log("Cart:", products);
-});
+    updateCart();
+}
+
+function updateCart() {
+
+    let products = $.cookie(cookieName); 
+
+    if (products === undefined) {
+        products = [];
+    }
+    else {
+        products = JSON.parse(products);
+    }
+
+    $("#cart_items_count").text(products.length);
+
+    let cartItemsWrapper = $("#cart_items_wrapper");
+
+    cartItemsWrapper.html("");
+
+    products.forEach(x => {
+
+        const product = `<div class="product">
+            <div class="product-cart-details">
+                <h4 class="product-title">
+                    <a href="product.html">${x.bookTitle}</a>
+                </h4>
+
+                <span class="cart-product-info">
+                    <span class="cart-product-qty">${x.count} x </span>
+                    ${x.price}
+                </span>
+            </div>
+
+            <figure class="product-image-container">
+                <a href="product.html" class="product-image">
+                    <img src="/Pictures/${x.pictureFile}" alt="محصول">
+                </a>
+            </figure>
+
+            <a href="#" class="btn-remove" title="حذف محصول">
+                <i class="icon-close"onclick="removeFromCart('${x.id}')" ></i>
+            </a>
+        </div>`;
+
+        cartItemsWrapper.append(product);
+    });
+}
+function removeFromCart(id) {
+    let products = $.cookie(cookieName);
+
+    if (products === undefined) {
+        products = [];
+    }
+    else {
+        products = JSON.parse(products);
+    }
+    let itemToRemove = products.findIndex(x => x.id === id);
+    products.splice(itemToRemove, 1);
+
+    $.cookie(cookieName, JSON.stringify(products), {
+        expires: 2,
+        path: "/"
+    });
+    updateCart();
+}
